22d42426d3aebb84b7f861c0ffedbd6f0948fa76
Namdak Tonpa <maxim@synrc.com>
2019-04-13 20:19:54 +0300

    dump

--- include/backend.hrl
+++ include/backend.hrl
@@ -1,6 +1,6 @@
 -ifndef(BACKEND_HRL).
 -define(BACKEND_HRL, true).
--define(BACKEND, [get/2,put/1,delete/2,index/3,
+-define(BACKEND, [get/2,put/1,delete/2,index/3,dump/0,
                   join/1,dir/0,create_table/2,add_table_index/2,seq/2,all/1,count/1,version/0]).
 -spec put(Record :: tuple()) -> ok | {error,any()}.
 -spec get(Tab :: atom(), Key :: any()) -> {ok,any()} | {error,not_found | duplicated}.
