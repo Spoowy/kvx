0fe8b73c6c184e457df6c7169a9c7c489c999399
Namdak Tonpa <maxim@synrc.com>
2019-04-15 13:31:57 +0300

    kvx_stream is new layer

--- include/stream.hrl
+++ include/stream.hrl
@@ -1,6 +1,17 @@
 -ifndef(STREAM_HRL).
 -include("kvx.hrl").
 -define(STREAM_HRL, true).
+-record(writer, { id    = [] :: term(),
+                  count =  0 :: integer(),
+                  cache = [] :: [] | tuple(),
+                  args  = [] :: term(),
+                  first = [] :: [] | tuple() } ).
+-record(reader, { id    = [] :: integer(),
+                  pos   =  0 :: [] | integer(),
+                  cache = [] :: [] | integer(),
+                  args  = [] :: term(),
+                  feed  = [] :: term(),
+                  dir   =  0 :: 0 | 1 } ).
 -define(STREAM, [top/1, bot/1, next/1, prev/1, drop/1, take/1,
                  load_writer/1, load_reader/1, writer/1, reader/1,
                  save/1, up/1, down/1, add/1]).
