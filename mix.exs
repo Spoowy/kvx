cf878d8f7eb4a0c5fa22fd850614419efde13881
Namdak Tonpa <maxim@synrc.com>
2019-04-19 15:32:07 +0300

    mix

--- mix.exs
+++ mix.exs
@@ -4,12 +4,12 @@ defmodule KVX.Mixfile do
   def project do
     [app: :kvx,
      version: "6.4.0",
-     description: "Erlang Abstract Database",
+     description: "Abstract Chain Database",
      package: package()]
   end
 
   defp package do
-    [files: ~w(include priv man etc src LICENSE mix.exs README.md rebar.config sys.config),
+    [files: ~w(include man etc src LICENSE mix.exs README.md rebar.config sys.config),
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/synrc/kvx"}]
    end
